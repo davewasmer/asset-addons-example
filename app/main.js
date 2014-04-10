792e3eabe9899256e1dd43ec96c9c694cd49a868
Dave Wasmer <davewasmer@gmail.com>
2014-04-09 23:03:15 -0400

    Tweaked initializer loading to use cascade lookup
    The `main.js` bootstrapping script seems to hack around the resolver by
    manually checking the internal registry of the module loader for any
    initializer modules. This was the only place I could see that didn't
    use the Resolver for lookup, so the only other place that needed to be
    tweaked to accomodate the cascade logic.
    
    Addon initializers are a good place to do things like patch core files
    (which the Resolver won't automatically pick up, i.e. patching
    Ember.Route for default authentication redirects on all routes) and for
    setting up routes in the Router.

--- app/main.js
+++ app/main.js
@@ -5,7 +5,13 @@ export default function bootApp(prefix, attributes) {
   var initializersRegExp = new RegExp(prefix + '/initializers');
 
   Ember.keys(requirejs._eak_seen).filter(function(key) {
-    return initializersRegExp.test(key);
+    var prefixes = [prefix].concat(ENV.cascade)
+    for (var i=0; i < prefixes.length; i++) {
+      var cascadePrefix = prefixes[i]
+      if (new RegExp(cascadePrefix + '/initializers').test(key)) {
+        return true;
+      }
+    }
   }).forEach(function(moduleName) {
     var module = require(moduleName, null, null, true);
     if (!module) { throw new Error(moduleName + ' must export an initializer.'); }
